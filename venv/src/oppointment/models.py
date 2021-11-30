57fde88d95839302a5625dfb3a34a08cfb129b5c
Siddhesh Kamath <63893110+PSYCHOSIDX@users.noreply.github.com>
2021-11-30 22:57:36 +0530

    ..

--- venv/src/oppointment/models.py
+++ venv/src/oppointment/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+# Basic User 
+
+class User(models.Model):
+     id = models.AutoField
+     email = models.CharField(max_length=200)
+     password = models.CharField(max_length=300)
+     username = models.CharField(max_length=200)
+
+class Admin(models.Model):
+     id = models.AutoField
+     email = models.CharField(max_length=200)
+     password = models.CharField(max_length=300)
+     username = models.CharField(max_length=200)
+     cid = models.IntegerField
