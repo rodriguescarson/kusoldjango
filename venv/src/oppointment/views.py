53f471ffd7396ac26e0040bc1ca45d3a475d3d28
Jainam R <jainam.rambhiar@gmail.com>
2021-12-17 12:32:11 +0530

    extract jwt token

--- venv/src/oppointment/views.py
+++ venv/src/oppointment/views.py
@@ -31,18 +31,50 @@ class RegisterView(APIView):
 
 class LoginView(APIView):
     def post(self, request):
-        email = request.data["email"]
-        password = request.data["password"]
+        email = request.data['email']
+        password = request.data['password']
 
-        user = User.objects.filter(
-            email=email
-        ).first()  # User is the collection here ok, should i copy this api code and make changes? wait
+        user = User.objects.filter(email=email).first()
+        print(user)
         if user is None:
-            raise AuthenticationFailed("User not found")
+            raise AuthenticationFailed('User not found')
 
         if not user.check_password(password):
-            raise AuthenticationFailed("Incorrect Password")
-        return Response({"message": "success"})
+            raise AuthenticationFailed('Incorrect Password')
+
+        payload = {
+            'id' : user.id,
+            'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat' : datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret' , algorithm = 'HS256').decode('utf-8')
+        print(token)
+        
+        response = Response()
+        response.set_cookie(key='jwt',value=token,httponly=True)
+        response.data = {
+            'jwt' : token
+        }
+
+        return response
+
+
+class UserView(APIView):
+    def get(self,request):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Unauthenticated')
+        
+        try:
+            payload = jwt.decode(token,'secret',algorithm = ['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated')
+        
+        user = User.objects.filter(id = payload['id']).first()
+        serializer= UserSerializer(user)
+
+        return Response(serializer.data)
 
 
 @api_view(["GET", "POST", "DELETE"])
